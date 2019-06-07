11897e40a41eadb5cda8bccb5d24aa29bf73601e
Kumar <rajeev.r.a.kumar@accenture.com>
2019-06-07 19:24:23 +0530

    Ngentest with API call

--- index.js
+++ index.js
@@ -19,48 +19,68 @@ const argv = yargs.usage('Usage: $0 <angular-typescript-file> [options]')
   .example('$0 my.component.ts', 'generate Angular unit test for my.component.ts')
   .help('h')
   .argv;
+  let methodCount;
+  if(argv !== undefined){
+    generateTestCase(argv,null);
+  } 
+  async function  generateTestCase(argv,fileName){
+	 if( (argv !== null && !argv.spec) && (fileName == null || fileName == undefined)){
+      return false;
+   }
+  const tsFile = argv !== null && argv._ !=''? '' + argv._ : fileName;
+  const typescript = fs.readFileSync(path.resolve(tsFile), 'utf8');
+  const specFilePath = path.resolve(tsFile.replace(/\.ts$/, '.spec.ts'));
 
-const tsFile = '' + argv._;
-const typescript = fs.readFileSync(path.resolve(tsFile), 'utf8');
-const specFilePath = path.resolve(tsFile.replace(/\.ts$/, '.spec.ts'));
+  const existingItBlocks = {};
+  if (((argv !== null && argv.spec) || fileName !== null ) && fs.existsSync(specFilePath)) {
+    const tests = fs.readFileSync(specFilePath, 'utf8');
+    (tests.match(/  it\('.*?',.*?\n  }\);/gs) || []).forEach(itBlock => {
+      const key = itBlock.match(/it\('(.*?)',/)[1];
+	    existingItBlocks[key] = `\n${itBlock}\n`;
+    });
+  }
+  parseTypescript(typescript).then(tsParsed => {
+    methodCount = 0;
+    const angularType = util.getAngularType(typescript); // Component, Directive, Injectable, Pipe, or undefined
+    const ejsTemplate = util.getEjsTemplate(angularType);
 
-const existingItBlocks = {};
-if (argv.spec && fs.existsSync(specFilePath)) {
-  const tests = fs.readFileSync(specFilePath, 'utf8');
-  (tests.match(/  it\('.*?',.*?\n  }\);/gs) || []).forEach(itBlock => {
-    const key = itBlock.match(/it\('(.*?)',/)[1];
-    existingItBlocks[key] = `\n${itBlock}\n`;
+    let ejsData;
+    switch(angularType) {
+      case 'Component':
+      case 'Directive':
+        ejsData = getDirectiveData(tsParsed, tsFile, angularType);
+        break;
+      case 'Injectable':
+        ejsData = getInjectableData(tsParsed, tsFile);
+        break;
+      case 'Pipe':
+        ejsData = getPipeData(tsParsed, tsFile);
+        break;
+      default:
+        ejsData = getDefaultData(tsParsed, tsFile);
+        break;
+    }
+    ejsData.functionTests = Object.assign({}, ejsData.functionTests, existingItBlocks);
+    methodCount = Object.keys(ejsData.functionTests).length;
+	  methodCount = methodCount+1;
+    const generated = ejs.render(ejsTemplate, ejsData).replace(/\n\s+$/gm, '\n');
+    if (argv !== null && argv.spec || fileName !== null) {
+      fs.existsSync(specFilePath);
+      fs.writeFileSync(specFilePath, generated);
+      console.log('Generated unit test for',  argv!== null ?argv._[0] : fileName, 'to', specFilePath);
+    } else {
+      process.stdout.write(generated);
+    }
   });
-}
+ }
 
-parseTypescript(typescript).then(tsParsed => {
-  const angularType = util.getAngularType(typescript); // Component, Directive, Injectable, Pipe, or undefined
-  const ejsTemplate = util.getEjsTemplate(angularType);
 
-  let ejsData;
-  switch(angularType) {
-    case 'Component':
-    case 'Directive':
-      ejsData = getDirectiveData(tsParsed, tsFile, angularType);
-      break;
-    case 'Injectable':
-      ejsData = getInjectableData(tsParsed, tsFile);
-      break;
-    case 'Pipe':
-      ejsData = getPipeData(tsParsed, tsFile);
-      break;
-    default:
-      ejsData = getDefaultData(tsParsed, tsFile);
-      break;
-  }
-  ejsData.functionTests = Object.assign({}, ejsData.functionTests, existingItBlocks);
+module.exports = async function(fileName) {
+   generateTestCase(null,fileName);
+   await delay(1);
+  return methodCount;
+};
 
-  const generated = ejs.render(ejsTemplate, ejsData).replace(/\n\s+$/gm, '\n');
-  if (argv.spec) {
-    fs.existsSync(specFilePath);
-    fs.writeFileSync(specFilePath, generated);
-    console.log('Generated unit test for', argv._[0], 'to', specFilePath);
-  } else {
-    process.stdout.write(generated);
-  }
-});
+let delay  = (seconds) => new Promise((resolve)=>{
+  setTimeout(resolve,seconds*1000)
+})
